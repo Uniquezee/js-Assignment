217c0dc01d471d771e3603c1b67da8b524164b3a
uniquezee <priscaonyemaobiamarachi@gmail.com>
2021-07-14 04:57:50 +0100

    js Assignment

---
+++ index.js
@@ -0,0 +1,79 @@
+/*var carName="Volvo";
+console.log(carName);
+
+let a, b, c;
+a = 5;
+b = 6;
+c = a + b;
+console.log(c)
+
+var x, y;
+x = 5 + 6;
+y = x * 10;
+console.log(y)
+ let lastname, lastName;
+ lastname = "Cletus";
+ lastName = "Onyemaobi";
+ console.log(lastName)
+
+ var x = 5;
+ var y = 6;
+ var z = x + y;
+ console.log(z);
+
+ var x = "5" + 2 + 3;
+ console.log(x)
+
+ var x = 2 + 3 + "5";
+ console.log(x)
+
+ var _lastName = "Onyemaobi";
+ var _x = 2;
+ var _y = _lastName +  _x
+ console.log(_y)
+ {
+     var x = 2;
+ }
+ var x = 5;
+ console.log(x)*/
+
+ //Assignment//
+
+ let array1 = [20,46,90,80,120];
+ let array2 = [2, 2, 5, 4, 2]
+ array3 =[[20/2],[46/2],[90/5],[80/4],[120/2]];
+ console.log(array3)
+
+ var arr = [[1, 2, 3],[4, 5, 6],7,8,9,[[10, 11, 12, [13, 4, 112]], 136, 14],];
+ var myData = arr[5][0][0];
+ console.log(myData);
+ var myData1 = arr[3];
+ console.log(myData1);
+ var myData2 = arr[5][0][3][0];
+ console.log(myData2);
+ var myData3 = arr[5][1];
+ console.log(myData3);
+ var myData4 = arr[5][0][3][1];
+ console.log(myData4);
+ var myData5 = arr[1][0]
+ console.log(myData5)
+  
+ var sentence = ["javascript", "web","Capacity Bay"] 
+ var ans1 = sentence[0];
+ console.log(ans1);
+ var ans2 = sentence[1];
+ console.log(ans2);
+ var ans3 = sentence[2];
+ console.log(ans3);
+ var finalAns = "I am a \"web\" developer at \"Capacity Bay\" and I love \"JavaScript\" ";
+ console.log(finalAns);
+
+ var character ="Generalization";
+ var ans1 = character[character.length-4];
+ console.log(ans1);
+ var fruits =["orange","apple","guava","lemon"];
+ console.log(fruits);
+ fruits.unshift("pea","banana");
+ console.log(fruits);
+ fruits.push("Avocado","mango");
+ console.log(fruits)
